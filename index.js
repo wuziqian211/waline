0ed20d88c8bca12728928033b16e6d23ae5cf17b
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2021-11-21 11:29:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Waline = require('@waline/vercel');
 module.exports = Waline({
   async preSave(comment) {
     if (/^[0-9]+$/.test(comment.link)) {
-      return { errmsg: comment.link };
+      return { errmsg: comment.nick };
       comment.link = 'https://space.bilibili.com/' + comment.link;
     }
   },
