0201098dc2052edc101ecb19246201db6d1a2e09
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2021-11-21 12:29:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const Waline = require('@waline/vercel');
-const fetch = require('node-fetch');
 
 module.exports = Waline({
   async preSave(comment) {
     if (/^[0-9]+$/.test(comment.link)) {
-      if (comment.nick == '' || comment.nick == '匿名')
-        fetch(`https://api.wuziqian211.top/api/getbili?mid=${comment.link}`).then(resp => resp.json()).then(json => comment.nick = json.data.name);
+      if (comment.nick == '' || comment.nick == '匿名') {
+        const fetch = require('node-fetch');
+        await fetch(`https://api.wuziqian211.top/api/getbili?mid=${comment.link}`).then(resp => resp.json()).then(json => comment.nick = json.data.name);
+      }
       comment.link = 'https://space.bilibili.com/' + comment.link;
     }
   },
