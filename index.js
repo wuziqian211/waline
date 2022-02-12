05d72fce9d893ed0685e20badfcb03481bb86fa3
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2022-02-12 15:03:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = Waline({
     }
     if ((!comment.nick || comment.nick === '匿名') && comment.link && comment.link.slice(0, 27) === 'https://space.bilibili.com/') {
       let uid = comment.link.slice(27);
-      if (/^[0-9]+$/.test(uid)) {
+      if (/^\d+$/.test(uid)) {
         await fetch('https://api.bilibili.com/x/space/acc/info?mid=' + uid).then(resp => resp.json()).then(json => comment.nick = json.code === 0 ? json.data.name : '');
         if (!comment.nick) {
           return {errmsg: '您输入的 UID 对应的用户可能不存在哦(´；ω；`)如果存在，那就重试一下吧awa'};
@@ -18,7 +18,7 @@ module.exports = Waline({
   async avatarUrl(comment) {
     if (comment.link && comment.link.slice(0, 27) === "https://space.bilibili.com/") {
       let uid = comment.link.slice(27);
-      if (/^[0-9]+$/.test(uid)) {
+      if (/^\d+$/.test(uid)) {
         return `https://api.wuziqian211.top/api/getbili?mid=${uid}`;
       }
     } else if (!comment.mail) {
