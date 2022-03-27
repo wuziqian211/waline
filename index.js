5e82c6ae26c58b5cc6523d8ead7e50cf0c9bb615
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2022-03-28 00:24:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Waline = require('@waline/vercel');
 const fetch = require('node-fetch');
+const md5 = require('md5');
 module.exports = Waline({
   async preSave(comment) {
     if (/^\d+$/.test(comment.link)) {
@@ -8,7 +9,8 @@ module.exports = Waline({
     if ((!comment.nick || comment.nick === '匿名') && comment.link?.slice(0, 27) === 'https://space.bilibili.com/') {
       let uid = comment.link.slice(27);
       if (/^\d+$/.test(uid)) {
-        await fetch('https://api.bilibili.com/x/space/acc/info?mid=' + uid).then(resp => resp.json()).then(json => comment.nick = json.code === 0 ? json.data.name : '');
+        const json = await (await fetch(`https://api.bilibili.com/x/space/acc/info?mid=${uid}`)).json();
+        comment.nick = json.code === 0 ? json.data.name : '';
         if (!comment.nick) {
           return {errmsg: '您输入的 UID 对应的用户可能不存在哦(´；ω；`)如果存在，那就重试一下吧awa'};
         }
@@ -21,7 +23,9 @@ module.exports = Waline({
       if (/^\d+$/.test(uid)) {
         return `https://api.wuziqian211.top/api/getuser?mid=${uid}`;
       }
-    } else if (!comment.mail) {
+    } else if (comment.mail) {
+      return `https://cravatar.cn/avatar/${md5(comment.mail)}?d=retro`;
+    } else {
       return 'https://api.wuziqian211.top/api/getuser';
     }
   }
