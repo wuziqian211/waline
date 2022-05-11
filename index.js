e1d38f415beb1e7e6e4616a75b521a1a6d9614d0
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2022-05-12 00:18:24 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,19 +7,20 @@ module.exports = Waline({
       comment.link = 'https://space.bilibili.com/' + comment.link;
     }
     if ((!comment.nick || comment.nick === '匿名') && comment.link?.slice(0, 27) === 'https://space.bilibili.com/') {
-      let uid = comment.link.slice(27);
+      const uid = comment.link.slice(27);
       if (/^\d+$/.test(uid)) {
         const json = await (await fetch(`https://api.bilibili.com/x/space/acc/info?mid=${uid}`)).json();
-        comment.nick = json.code === 0 ? json.data.name : '';
-        if (!comment.nick) {
-          return {errmsg: '您输入的 UID 对应的用户可能不存在哦(´；ω；`)如果存在，那就重试一下吧awa'};
+        if (json.code === 0) {
+          comment.nick = json.data.name;
+        } else {
+          return {errmsg: '您输入的 UID 对应的用户可能不存在哦(´；ω；`)如果存在，那就重试一下吧 awa'};
         }
       }
     }
   },
   async avatarUrl(comment) {
     if (comment.link?.slice(0, 27) === 'https://space.bilibili.com/') {
-      let uid = comment.link.slice(27);
+      const uid = comment.link.slice(27);
       if (/^\d+$/.test(uid)) {
         return `https://api.wuziqian211.top/api/getuser?mid=${uid}`;
       }
