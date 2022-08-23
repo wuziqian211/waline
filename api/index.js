b3df156f4b95ed170577ad1ea474679ac1a3e895
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2022-08-24 00:10:33 +0800

    update

--- api/index.js
+++ api/index.js
@@ -4,28 +4,23 @@ module.exports = Waline({
     if (/^\d+$/.test(comment.link)) {
       comment.link = 'https://space.bilibili.com/' + comment.link;
     }
-    if ((!comment.nick || comment.nick === '匿名') && comment.link?.slice(0, 27) === 'https://space.bilibili.com/') {
-      const uid = comment.link.slice(27);
-      if (/^\d+$/.test(uid)) {
-        const json = await (await fetch(`https://api.bilibili.com/x/space/acc/info?mid=${uid}`)).json();
-        if (json.code === 0) {
-          comment.nick = json.data.name;
-        } else {
-          return {errmsg: '您输入的 UID 对应的用户可能不存在哦(´；ω；`)如果存在，那就重试一下吧 awa'};
-        }
+    if ((!comment.nick || comment.nick === '匿名') && /^https:\/\/space\.bilibili\.com\/\d+$/.test(comment.link)) {
+      const json = await (await fetch(`https://api.bilibili.com/x/space/acc/info?mid=${comment.link.slice(27)}`)).json();
+      if (json.code === 0) {
+        comment.nick = json.data.name;
+      } else {
+        return { errmsg: '您输入的 UID 对应的用户可能不存在哦(´；ω；`)如果存在，就重试一下吧 awa' };
       }
     }
   },
   async avatarUrl(comment) {
-    if (comment.link?.slice(0, 27) === 'https://space.bilibili.com/') {
-      const uid = comment.link.slice(27);
-      if (/^\d+$/.test(uid)) {
-        return `https://api.wuziqian211.top/api/getuser?mid=${uid}`;
-      }
+    if (/^https:\/\/space\.bilibili\.com\/\d+$/.test(comment.link)) {
+      return `https://api.wuziqian211.top/api/getuser?mid=${comment.link.slice(27)}`;
     } else if (comment.mail) {
       return `https://cravatar.cn/avatar/${md5(comment.mail)}?d=retro`;
     } else {
-      return 'https://api.wuziqian211.top/api/getuser';
+      const faces = ['1-22', '1-33', '2-22', '2-33', '3-22', '3-33', '4-22', '4-33', '5-22', '5-33', '6-33'];
+      return `https://api.wuziqian211.top/assets/${faces[Math.floor(Math.random() * 11)]}.jpg`;
     }
   }
 });
