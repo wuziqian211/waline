14f0770bc3d962f3c94c5a345b6a3bd396a5f9f6
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2024-02-07 01:24:17 +0800

    revert & update

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@
 const Waline = require('@waline/vercel'), md5 = require('md5');
 module.exports = Waline({
   plugins: [],
+/*
   async preSave(comment) {
     if (/^\d+$/.test(comment.link.trim())) {
       comment.link = 'https://space.bilibili.com/' + comment.link.trim();
@@ -30,4 +31,5 @@ module.exports = Waline({
       return `/images/default-faces%26face-icons/${faces[Math.floor(Math.random() * 11)]}.jpg`;
     }
   }
+*/
 });
