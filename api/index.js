cc8bfd464ded3a73bf71ae4690280514203f5a7d
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2024-02-06 22:44:06 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,7 +7,6 @@ module.exports = Waline({
       comment.link = 'https://space.bilibili.com/' + comment.link.trim();
     }
     if ((!comment.nick.trim() || comment.nick === '匿名') && /^(?:(?:https?:)?\/\/)?space\.bilibili\.com\/\d+(?:[\?\/#].*)?$/i.test(comment.link.trim())) {
-      comment.link = comment.link.trim().replace(/^(?:(?:https?:)?\/\/)?space\.bilibili\.com\/(\d+(?:[\?\/#].*)?)$/i, 'https://space.bilibili.com/$1');
       const json = await (await fetch(`https://account.bilibili.com/api/member/getCardByMid?mid=${comment.link.trim().replace(/^(?:(?:https?:)?\/\/)?space\.bilibili\.com\/(\d+)(?:[\?\/#].*)?$/i, '$1')}`, { headers: { Origin: 'https://space.bilibili.com', Referer: 'https://space.bilibili.com/', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36' } })).json();
       if (json.code === 0) {
         comment.nick = json.card.name;
@@ -18,12 +17,12 @@ module.exports = Waline({
   },
   async avatarUrl(comment) {
     if (/^(?:(?:https?:)?\/\/)?space\.bilibili\.com\/\d+(?:[\?\/#].*)?$/i.test(comment.link.trim())) {
-      return `https://api.yumeharu.top/api/getuser?mid=${comment.link.trim().replace(/^(?:(?:https?:)?\/\/)?space\.bilibili\.com\/(\d+)(?:[\?\/#].*)?$/i, '$1')}&type=avatar`;
+      return `https://api.yumeharu.top/api/getuser?mid=${comment.link.trim().replace(/^(?:(?:https?:)?\/\/)?space\.bilibili\.com\/(\d+)(?:[\?\/#].*)?$/i, '$1')}&type=avatar_redirect`;
     } else if (comment.mail) {
       return `https://cravatar.cn/avatar/${md5(comment.mail)}?d=retro`;
     } else {
       const faces = ['1-22', '1-33', '2-22', '2-33', '3-22', '3-33', '4-22', '4-33', '5-22', '5-33', '6-33'];
-      return `https://api.yumeharu.top/assets/${faces[Math.floor(Math.random() * 11)]}.jpg`;
+      return `/images/default-faces%26face-icons/${faces[Math.floor(Math.random() * 11)]}.jpg`;
     }
   }
 });
