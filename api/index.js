288e94962c674bd30b5a7b7b36cb11a42f1c54de
wuziqian211 <65224318+wuziqian211@users.noreply.github.com>
2024-03-03 22:38:51 +0800

    fix 'undefined' bug

--- api/index.js
+++ api/index.js
@@ -4,10 +4,10 @@ const Waline = require('@waline/vercel'), crypto = require('node:crypto'), md5 =
 module.exports = Waline({
   plugins: [],
   async preSave(comment) {
-    if (/^\d+$/.test(comment.link.trim())) {
+    if (comment.link && /^\d+$/.test(comment.link.trim())) {
       comment.link = 'https://space.bilibili.com/' + comment.link.trim();
     }
-    if ((!comment.nick.trim() || comment.nick === '匿名') && /^(?:(?:https?:)?\/\/)?space\.bilibili\.com\/\d+(?:[\?\/#].*)?$/i.test(comment.link.trim())) {
+    if (comment.link && (!comment.nick?.trim() || comment.nick === '匿名') && /^(?:(?:https?:)?\/\/)?space\.bilibili\.com\/\d+(?:[\?\/#].*)?$/i.test(comment.link.trim())) {
       const json = await (await fetch(`https://account.bilibili.com/api/member/getCardByMid?mid=${comment.link.trim().replace(/^(?:(?:https?:)?\/\/)?space\.bilibili\.com\/(\d+)(?:[\?\/#].*)?$/i, '$1')}`, { headers: { Origin: 'https://space.bilibili.com', Referer: 'https://space.bilibili.com/', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36' } })).json();
       if (json.code === 0) {
         comment.nick = json.card.name;
@@ -15,7 +15,7 @@ module.exports = Waline({
         return { errmsg: '您输入的 UID 对应的用户可能不存在哦 (´；ω；`) 如果存在，就重试一下吧 awa' };
       }
     }
-    if (/^\d+@qq\.com$/i.test(comment.mail.trim())) {
+    if (comment.mail && /^\d+@qq\.com$/i.test(comment.mail.trim())) {
       const qqNumber = comment.mail.trim().replace(/^(\d+)@qq\.com$/i, '$1'),
         hashes = await kv.get('hashes');
       if (!hashes.some(h => h.s === qqNumber)) {
@@ -25,9 +25,9 @@ module.exports = Waline({
     }
   },
   async avatarUrl(comment) {
-    if (/^(?:(?:https?:)?\/\/)?space\.bilibili\.com\/\d+(?:[\?\/#].*)?$/i.test(comment.link.trim())) {
+    if (comment.link && /^(?:(?:https?:)?\/\/)?space\.bilibili\.com\/\d+(?:[\?\/#].*)?$/i.test(comment.link.trim())) {
       return `https://api.yumeharu.top/api/getuser?mid=${comment.link.trim().replace(/^(?:(?:https?:)?\/\/)?space\.bilibili\.com\/(\d+)(?:[\?\/#].*)?$/i, '$1')}&type=avatar_redirect`;
-    } else if (comment.mail.trim()) {
+    } else if (comment.mail?.trim()) {
       if (/^\d+@qq\.com$/i.test(comment.mail.trim())) {
         const qqNumber = comment.mail.trim().replace(/^(\d+)@qq\.com$/i, '$1'),
           hashes = await kv.get('hashes');
